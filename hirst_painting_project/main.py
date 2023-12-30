49869c0b99aacfe8210bb34fd0529026d7778ee3
J Garcia <102822083+Br0wnB0mber0@users.noreply.github.com>
2023-12-29 21:09:27 -0600

    added some comment code so I can make heads or tails of this later

--- hirst_painting_project/main.py
+++ hirst_painting_project/main.py
@@ -1,5 +1,6 @@
 import colorgram
 
+# I used this to have the program look at the painting, then pull the colors RGB values and add them to a list called rgb_colors
 # rgb_colors = []
 # colors = colorgram.extract('image.jpg', 30)
 # for color in colors:
@@ -13,16 +14,19 @@ import colorgram
 from turtle import Turtle, Screen
 import turtle
 import random
+# Creates a turtle object
 timmy = Turtle()
 # This lets us set the colormode to accept RBG tuples AND strings to set colors
 turtle.colormode(255)
 
 color_list = [(202, 164, 110), (149, 75, 50), (222, 201, 136), (53, 93, 123), (170, 154, 41), (138, 31, 20), (134, 163, 184), (197, 92, 73), (47, 121, 86), (73, 43, 35), (145, 178, 149), (14, 98, 70), (232, 176, 165), (160, 142, 158), (54, 45, 50), (101, 75, 77), (183, 205, 171), (36, 60, 74), (19, 86, 89), (82, 148, 129), (147, 17, 19), (27, 68, 102), (12, 70, 64), (107, 127, 153), (176, 192, 208), (168, 99, 102)]
-
+# Pulls the turtle's pen up so it doesn't ink while getting to the defined starting point
 timmy.penup()
+# Makes the turtle go super fast so I don't have to wait to see if I did it right
 timmy.speed("fastest")
 
-
+# Function that draws a dotted line, with dots instead of dashes. 
+# Sends Timmy to the bottom left of the screen, draws a dot in a random color, goes pen up, then moves him in increments of 50 pixels
 def draw_dot_line():
     y_coordinate = -200
     timmy.setpos(-200, y_coordinate)
@@ -35,8 +39,11 @@ def draw_dot_line():
             timmy.penup()
             timmy.forward(50)
 
-
+# Calls the function that draws a dotted line
 draw_dot_line()
+# Hides the turtle
 timmy.hideturtle()
+# Creates the screen object
 screen = Screen()
+# Keeps the screen open until we click, so I can see the beautiful artwork
 screen.exitonclick()
