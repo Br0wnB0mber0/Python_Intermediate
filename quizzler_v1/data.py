5e73e9154d6caffec7ed7d85e93f0236a0921de7
J Garcia <102822083+Br0wnB0mber0@users.noreply.github.com>
2023-12-29 21:32:58 -0600

    Comment code makes me happy

--- quizzler_v1/data.py
+++ quizzler_v1/data.py
@@ -1,11 +1,15 @@
 import requests
 # You can add a "category" by adding that key to the parameters dictionary with the category number you can pull from the API.
 # Category 18 is computer related questions
+# Parameters for the request we are going to send the API
 parameters = {
     "amount": 10,
     "type": "boolean"
 }
+# Response we get from the Open Trivia Database, using the API URL and the parameters as defined above
 response = requests.get(url="https://opentdb.com/api.php?", params=parameters)
+# Raises an exception if there is a problem with the response
 response.raise_for_status()
+# Stores the JSON data with the results of the response. Here, the results key is the question and answer data we pull from the API
 question_data = response.json()['results']
 
