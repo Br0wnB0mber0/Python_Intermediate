2fd72a443f531ac606badc5ba726c4fdb32fdc28
J Garcia <102822083+Br0wnB0mber0@users.noreply.github.com>
2023-12-29 21:55:43 -0600

    At long last, comment code

--- quizzler_v1/quiz_brain.py
+++ quizzler_v1/quiz_brain.py
@@ -1,16 +1,19 @@
 import html
-
+# Creates the QuizBrain class, requires a list parameter to be passed in during the object creation
 class QuizBrain:
 
     def __init__(self, q_list):
+        # Defines the question number, score, current question, and takes the list that was passed in and sets the self.question_list variable
         self.question_number = 0
         self.score = 0
         self.question_list = q_list
         self.current_question = None
 
+    # If the question number is less that the length of the question list, returns True
     def still_has_questions(self):
         return self.question_number < len(self.question_list)
 
+    # Function that creates a question for the quiz game
     def next_question(self):
         self.current_question = self.question_list[self.question_number]
         self.question_number += 1
@@ -19,6 +22,7 @@ class QuizBrain:
         # user_answer = input(f"Q.{self.question_number}: {q_text} (True/False): ")
         # self.check_answer(user_answer)
 
+    # Function that checks the user answer against the correct one and returns True or False, increasing the score as appropriate
     def check_answer(self, user_answer):
         correct_answer = self.current_question.answer
         if correct_answer == user_answer:
