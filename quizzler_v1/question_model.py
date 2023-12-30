72c8f41c78d2ebe36d14e9a3ef0ca776f8649765
J Garcia <102822083+Br0wnB0mber0@users.noreply.github.com>
2023-12-29 21:37:56 -0600

    comment code for the Question class

--- quizzler_v1/question_model.py
+++ quizzler_v1/question_model.py
@@ -1,3 +1,7 @@
+# Creates the question class. The object needs two parameters in the call to function:
+# The question text, which is the question from question_data in the data class
+# The question answer, also from question_data
+# Powers combined, this creates the Question object
 class Question:
 
     def __init__(self, q_text, q_answer):
